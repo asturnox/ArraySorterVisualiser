c4e360a2365cb018e8d9dbef9d202fdf747460da
Saturnox <napmaster47@gmail.com>
2021-11-22 22:47:57 +0100

    fix(Bar): update is_changed for every method

--- Bar.py
+++ Bar.py
@@ -13,8 +13,9 @@ class Bar(pygame.sprite.Sprite):
         super().__init__()
         self.value = value
         self.position = position
-        self.image = pygame.surface.Surface((canvas_width/config.num_values - padding, value * canvas_height/config.num_values))
-        self.image.fill((255, 255, 255))
+        self.image = pygame.surface.Surface(
+            (canvas_width / config.num_values - padding, value * canvas_height / config.num_values))
+        self.image.fill((255, 255, 255))  # set default colour to white
         self.image.convert()
         self.rect = self.image.get_rect()
         self.rect.bottomleft = ((canvas_width + padding) / config.num_values * position, canvas_height)
@@ -22,12 +23,13 @@ class Bar(pygame.sprite.Sprite):
         self.is_changed = True
 
     def update_position(self):
-        self.rect.bottomleft = ((canvas_width + padding) * self.position/config.num_values, canvas_height)
+        self.rect.bottomleft = ((canvas_width + padding) * self.position / config.num_values, canvas_height)
         self.swapped()
 
     def update_value(self, value):
         self.value = value
-        self.image = pygame.surface.Surface((canvas_width/config.num_values - padding, value * canvas_height/config.num_values))
+        self.image = pygame.surface.Surface(
+            (canvas_width / config.num_values - padding, value * canvas_height / config.num_values))
         self.rect = self.image.get_rect()
         self.rect.bottomleft = ((canvas_width + padding) / config.num_values * self.position, canvas_height)
         self.image.fill((255, 255, 255))
@@ -48,18 +50,19 @@ class Bar(pygame.sprite.Sprite):
         passed_noise(self.value)
 
     def selected(self):
+        self.is_changed = True
         self.image.fill((100, 0, 100))
 
     def pivot(self):
+        self.is_changed = True
         self.image.fill((0, 0, 255))
 
 
 def generate_bars():
-    print(config.num_values)
     arr = [i for i in range(config.num_values)]
     shuffle(arr)
     bar_array = [Bar(arr[i] + 1, i) for i in range(config.num_values)]
-    
+
     return bar_array
 
 
@@ -67,5 +70,3 @@ def swap(bar1, bar2):
     bar1.position, bar2.position = bar2.position, bar1.position
     bar1.update_position()
     bar2.update_position()
-
-
